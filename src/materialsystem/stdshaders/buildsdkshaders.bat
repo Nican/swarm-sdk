b14cfccc35854adb2de8fa8a02a64d1a7f0c014b
kosire.dk@gmail.com <kosire.dk@gmail.com@661475e6-acb8-01b8-bb7e-32fcb59befc8>
2012-07-20 03:29:21 +0000

    git-svn-id: http://swarm-sdk-template.googlecode.com/svn@67 661475e6-acb8-01b8-bb7e-32fcb59befc8

--- src/materialsystem/stdshaders/buildsdkshaders.bat
+++ src/materialsystem/stdshaders/buildsdkshaders.bat
@@ -52,8 +52,7 @@ set ARG_EXTRA=
 
 %BUILD_SHADER% stdshader_dx9_20b		-game %GAMEDIR% -source %SOURCEDIR%
 %BUILD_SHADER% stdshader_dx9_20b_new		-game %GAMEDIR% -source %SOURCEDIR% -dx9_30
-%BUILD_SHADER% stdshader_dx9_30			-game %GAMEDIR% -source %SOURCEDIR% -dx9_30	-force30 
-
+%BUILD_SHADER% stdshader_dx9_30		-game %GAMEDIR% -source %SOURCEDIR% -dx9_30	-force30 
 
 rem echo.
 if not "%dynamic_shaders%" == "1" (
